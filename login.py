45fde79c2ed9262ae6827cd022a78645d91da649
x64nik <rushidarunte123@gmail.com>
2023-03-04 19:01:07 +0530

    fix somting but dont know wht ;)

--- login.py
+++ login.py
@@ -1,20 +1,21 @@
-from flask import Flask, redirect, render_template, request, url_for
-from pymongo import MongoClient
-from bson.json_util import dumps
-import hash
+from flask import *
 
-app = Flask(__name__)
 
-client = MongoClient('192.168.0.106', 27017, username='nik', password='password')
-db = client.Project01
+client = MongoClient('192.168.0.103', 27017, username='nik', password='password')
+db = client.flask_db
 login_info = db.login_info
 
 @app.route('/')
+
 def index():
+    creds = login_info.find_one({'email': 'test@test.com'})
+    print(creds['email'])
+    print(login_info.find_one({'email': 'test@test.com'})['passwd_hash'])
     return render_template('index.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
+
 def login():
     if request.method == 'POST':
         email = request.form['email']
@@ -28,6 +29,7 @@ def login():
         
     
 @app.route('/signup', methods=['GET', 'POST'])
+
 def signup():
     if request.method == 'POST':
         email = request.form['email']
@@ -39,6 +41,3 @@ def signup():
             passwd_hash = hash.hash_creds(email, password)
             login_info.insert_one({'email': email, 'passwd_hash': passwd_hash})
         return redirect(url_for('index'))
-        
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
